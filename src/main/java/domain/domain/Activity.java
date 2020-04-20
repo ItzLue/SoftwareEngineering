1acd494b929928c53650cacea3bfda630d828870
Loui <S194294@student.dtu.dk>
2020-04-20 22:44:39 +0200

    From previous project

---
+++ src/main/java/domain/domain/Activity.java
@@ -0,0 +1,18 @@
+package domain.domain;
+
+import time.Interval;
+
+public class Activity {
+    protected String description;
+    protected String ID;
+    protected Interval interval;
+
+    public Activity() {
+
+    }
+
+    public String getID() {
+        return this.ID;
+    }
+
+}
---
+++ src/main/java/domain/domain/Project.java
@@ -0,0 +1,74 @@
+package domain.domain;
+
+import domain.Activity;
+import domain.Developer;
+import time.Interval;
+
+import java.util.ArrayList;
+
+public class Project {
+
+    protected Developer projectLeader;
+    protected ArrayList<Activity> activityList = new ArrayList<>();
+    protected boolean initialized;
+    protected Interval interval;
+    private String ID = "";
+    protected String name;
+
+    public Project(String name) {
+        this.name = name;
+        this.initialized = false;
+    }
+
+    public void addActivity(Activity activity) {
+        this.activityList.add(activity);
+    }
+
+    public void initProject() {
+        this.initialized = true;
+    }
+
+    public void setProjectLeader(Developer developer) {
+        if (initialized) {
+            this.projectLeader = developer;
+        }
+        else {
+            System.out.println("deeeet var vidste en ommerr ahahaha skal vi have en lille en: læst med arne nougatgren stemme");
+        }
+    }
+
+    public void setName(String name) { this.name = name; }
+
+    public void setID(String ID) {
+        if (this.ID.equals("")) {
+            this.ID = ID;
+        }
+    }
+
+    public String getID() {
+        return this.ID;
+    }
+
+    @Override
+    public String toString() {
+        return ", name:'" + name + '\'' +
+                ", ID: '" + ID + '\'' +
+                "projectLeader: " + projectLeader +
+                ", activityList: " + activityList;
+    }
+
+    public boolean isInitialized() {
+        return initialized;
+    }
+
+    public void setInterval(Interval interval) {
+        this.interval = interval;
+    }
+
+    public Interval getInterval() {
+        return this.interval;
+    }
+
+
+
+}
